fa06bb733e2b695c0da636551408757236bdae15
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2025-04-01 18:52:17 +0000

    conversion datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score=33
+console.log(typeof score);
+console.log(typeof(score));
+
+// "33"==>33
+// "33abc"==>NaN
+// true==>1;false==>0;
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1==>true ,0==>false,"" ==>false  "saurabh"==>true
+
+
+let someNumber=33
+let stringNumber=String(someNumber);//This line is conevert the someNumber in the String
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
